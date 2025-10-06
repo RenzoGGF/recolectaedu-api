5f2184b013d09f9db4ce386d52cd776a61f35acc
Seroluce <141604179+Seroluce@users.noreply.github.com>
2025-10-05 21:02:51 -0500

    feat(repository): añadir métodos para consultas por recurso y usuario en reseñas
    Se implementaron métodos en `ResenaRepository` para buscar reseñas por recurso, usuario y verificar la existencia de reseñas asociadas a ambos.

--- src/main/java/com/recolectaedu/repository/ResenaRepository.java
+++ src/main/java/com/recolectaedu/repository/ResenaRepository.java
@@ -1,7 +1,15 @@
 package com.recolectaedu.repository;
 
+import com.recolectaedu.model.Recurso;
 import com.recolectaedu.model.Resena;
+import com.recolectaedu.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ResenaRepository extends JpaRepository<Resena, Integer> {
+    List<Resena> findByRecurso(Recurso recurso);
+    List<Resena> findByUsuario(Usuario usuario);
+
+    boolean existsByUsuarioAndRecurso(Usuario usuario, Recurso recurso);
 }
