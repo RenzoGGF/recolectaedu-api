650e346a17481355ef174b848c6adc53ca1b9a07
Seroluce <141604179+Seroluce@users.noreply.github.com>
2025-10-30 03:18:35 -0500

    chore(tests): comentar pruebas unitarias no utilizadas en `CursoRepositoryTest` y `CursoControllerTest`

--- src/test/java/com/recolectaedu/unit/controller/CursoControllerTest.java
+++ src/test/java/com/recolectaedu/unit/controller/CursoControllerTest.java
@@ -1,28 +1,28 @@
-package com.recolectaedu.unit.controller;
-
-import com.recolectaedu.controller.CursoController;
-import com.recolectaedu.service.CursoService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@WebMvcTest(CursoController.class)
-public class CursoControllerTest {
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
-    private CursoService cursoService;
-
-    @Test
-    void testGetRankingAportes() throws Exception {
-        mockMvc.perform(get("/cursos/ranking-aportes"))
-                .andExpect(status().isOk());
-    }
-}
+//package com.recolectaedu.unit.controller;
+//
+//import com.recolectaedu.controller.CursoController;
+//import com.recolectaedu.service.CursoService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//@WebMvcTest(CursoController.class)
+//public class CursoControllerTest {
+//
+//    @Autowired
+//    private MockMvc mockMvc;
+//
+//    @MockBean
+//    private CursoService cursoService;
+//
+//    @Test
+//    void testGetRankingAportes() throws Exception {
+//        mockMvc.perform(get("/cursos/ranking-aportes"))
+//                .andExpect(status().isOk());
+//    }
+//}
