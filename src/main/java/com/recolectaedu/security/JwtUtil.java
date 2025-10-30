fd46ccdea593843227469ce184a53fdb5049b14b
Seroluce <141604179+Seroluce@users.noreply.github.com>
2025-10-30 03:18:46 -0500

    chore(tests): comentar pruebas unitarias no utilizadas en `CursoRepositoryTest` y `CursoControllerTest`

--- src/main/java/com/recolectaedu/security/JwtUtil.java
+++ src/main/java/com/recolectaedu/security/JwtUtil.java
@@ -2,13 +2,12 @@ package com.recolectaedu.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
-import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -21,15 +20,16 @@ public class JwtUtil {
     private Long expiration;
 
     // Generar token con email
-    public String generateToken(String email) {
+    public String generateToken(String email, String name) {
         Date now = new Date();
         Date expirationDate = new Date(now.getTime() + expiration);
 
         return Jwts.builder()
-                .setSubject(email)
-                .setIssuedAt(now)
-                .setExpiration(expirationDate)
-                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .subject(email)
+                .issuedAt(now)
+                .expiration(expirationDate)
+                .claim("name", name)
+                .signWith(getSigningKey(), Jwts.SIG.HS256) // infiere HS256
                 .compact();
     }
 
@@ -41,27 +41,27 @@ public class JwtUtil {
 
     // Obtener todos los claims
     private Claims getAllClaimsFromToken(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKey(getSigningKey())
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
                 .build()
-                .parseClaimsJws(token)
-                .getBody();
+                .parseSignedClaims(token)
+                .getPayload();
     }
 
     // Validar token
     public boolean validateToken(String token) {
         try {
-            Jwts.parserBuilder()
-                    .setSigningKey(getSigningKey())
+            Jwts.parser()
+                    .verifyWith(getSigningKey())
                     .build()
-                    .parseClaimsJws(token);
+                    .parseSignedClaims(token);
             return true;
         } catch (Exception e) {
             return false;
         }
     }
 
-    private Key getSigningKey() {
+    private SecretKey getSigningKey() {
         byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
         return Keys.hmacShaKeyFor(keyBytes);
     }
